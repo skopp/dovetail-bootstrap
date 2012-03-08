404e0d5ec1cd8433bb7650c7ff1a0c975c7528ef
KevM <scoundrel@gmail.com>
2012-03-08 10:01:01 -0600

    checkpoint ripplize

--- old-buildsupport/deploy.rb
+++ old-buildsupport/deploy.rb
@@ -1,12 +1,13 @@
 
 DOVETAIL_FEED = "http://focus.dovetailsoftware.com/nuget"
-NUGET_FEEDS.push(DOVETAIL_FEED + "/nuget")
+#NUGET_FEEDS.push(DOVETAIL_FEED + "/nuget")
 
 #desc "Deploy nuget packages to local feed (share)"
 task :deploy_nuget_packages do 
 	packagesDir = File.absolute_path("results/packages")
 	Dir.glob(File.join(packagesDir,"*.nupkg")){ |file|
 		puts "Deploying #{File.basename(file)} to #{DOVETAIL_FEED}"
-		sh "#{NUGET_EXE} push #{file.gsub(/\//,"\\\\")} -s #{DOVETAIL_FEED}"
+		#sh "#{NUGET_EXE} push #{file.gsub(/\//,"\\\\")} -s #{DOVETAIL_FEED}"
+		FileUtils::cp(file, 'c://nuget')
 	}
 end
